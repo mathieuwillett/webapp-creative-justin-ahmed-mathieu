acea05466cb0e670bbf621b4e015f8c0de29aca6
Foxyfox09 <2396998@cmontmorency.qc.ca>
2025-11-19 16:58:01 -0500

    Images + mort
    Ajout d'un filtre lors de la mort et d'un meilleur positionnement des personnages.

--- webapp_heros/src/components/histoire/PersonnagesBloc.vue
+++ webapp_heros/src/components/histoire/PersonnagesBloc.vue
@@ -1,55 +1,92 @@
 <script setup>
-import { usePlayerStore } from '@/stores/playerStore.js'
+import { usePlayerStore } from "@/stores/playerStore.js";
+const store = usePlayerStore();
 
-const store = usePlayerStore()
 </script>
+
+
 
 <template>
   <div class="screen">
-    <div class="wrapper">
-      <div 
+    <div class="groups">
+      <div class="wrapper">
+      <div
         class="personnages"
-        v-for="(personnage, index) in store.images"
+        v-for="(personnage, index) in store.images.filter(p => p.name !== 'Haxan')"
         :key="index"
       >
-        <img :src="personnage.img" :alt="personnage.name" />
+        <img :src="personnage.img" :alt="personnage.name" :class="{ dead: store.hp[personnage.name] <= 0 }"/>
       </div>
+    </div>
+    
+
+    <div class="wrapper">
+  <div class="personnages" v-for="(personnage, index) in store.images.filter(p => p.name == 'Haxan')"
+    :key="index">
+    <img :src="personnage.img" :alt="personnage.name" />
+  </div>
+</div>
+
+
+  
+
+      <!-- <div
+        class="haxan"
+        v-for="(personnage, index) in store.images"
+        :key="8"
+      > <img :src="personnage.img" :alt="personnage.name">
+    </div> -->
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .screen {
-    background-color: lightgray;
-    width: 100%;
-    height: 50vh;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    border: solid 2px black;
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
+  background-color: lightgray;
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: solid 2px black;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
 }
 
 .personnages {
-    background-color: red;
-    height: 20vh;
-    width: 155px;
-    border: solid 2px black;
-    
+  /* background-color: red; */
+  height: 12vw;
+  width: 8vw;
+   border: solid 2px black; 
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  margin: 0 0.2vw;
 }
 
 .wrapper {
-    display: flex;
-    justify-content: space-around;
-    bottom: 0;
-    width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  bottom: 0;
+  width: 100%;
+}
+
+.groups {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  bottom: 0;
+  width: 100%;
 }
 
 img {
   height: 100%;
-  width:auto;
+  width: auto;
+}
+
+img.dead {
+  filter: grayscale(100%) brightness(120%);
 }
 </style>
